3c94a08b1364af60fbfc953a4366c67f26811904
Vlad <me@vladbarsukov.ru>
2023-06-29 21:46:25 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import React, {useEffect} from 'react';
-import logo from './img/logoVladik.png'
+import React from 'react';
 import styles from './App.module.css'
 import './App.module.css';
-import MainCard from "./components/main-card/main-card";
-import {getAnimeSeason} from "./services/actions/animeSeasonActions";
-import {useDispatch} from "./services/hook";
-import CardList from "./components/card-list/card-list";
-import {BrowserRouter, Route, Router, Routes, useSearchParams} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./home/home";
 
 
 function App() {
-    const dispatch = useDispatch();
-
-    // useEffect(
-    //     () => {
-    //         dispatch(getAnimeSeason(1));
-    //     },
-    //     [dispatch]
-    // );
-
     return (
       <div className={styles.App}>
         <BrowserRouter>
             <Routes>
-                <Route path="/anime_list" element={<Home />} />
+                <Route path="/anime_list/" element={<Home />} />
                 <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
